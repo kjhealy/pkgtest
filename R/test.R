3ca24c2b0fc37fa8f7c3711ab2d88a42357c5f8c
Kieran Healy <kjhealy@gmail.com>
2022-04-29 10:14:36 -0400

    Initial commit

---
+++ R/test.R
@@ -0,0 +1,18 @@
+#' Add one
+#'
+#' Add one to some number or numbers
+#'
+#' @param x A vector
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+add_one <- function(x) {
+  x + 1
+}
